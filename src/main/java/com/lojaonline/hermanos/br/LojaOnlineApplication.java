0e8b3334380e5b252b7042dee49c05ff4130d328
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2023-05-17 22:16:33 -0300

    Init project

---
+++ src/main/java/com/lojaonline/hermanos/br/LojaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.lojaonline.hermanos.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojaonline/hermanos/br/LojaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaonline.hermanos.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
