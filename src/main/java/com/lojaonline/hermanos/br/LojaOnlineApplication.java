9999458929a7edc334399bd87b472b27bd880556
Kaian Luciano <kaianlucianoalvesdossantos@gmail.com>
2023-05-18 10:27:19 -0300

    Inseridos métodos HTTP (CRUD) para Pedidos

--- src/main/java/com/lojaonline/hermanos/br/LojaOnlineApplication.java
+++ src/main/java/com/lojaonline/hermanos/br/LojaOnlineApplication.java
@@ -3,6 +3,10 @@ package com.lojaonline.hermanos.br;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	Criador do Projeto: Kaian Luciano Alves Dos Santos
+	Projeto: Loja Online
+ */
 @SpringBootApplication
 public class LojaOnlineApplication {
 
