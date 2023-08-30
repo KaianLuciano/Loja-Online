e4c4806ca519220c2871fa3d61c196edf008a327
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-08-30 20:25:40 -0300

    Criado records DadosListagemProduto e DadosAtualizaProjeto

--- src/main/java/com/lojaonline/hermanos/br/models/dto/produto/DadosAtualizaProduto.java
+++ src/main/java/com/lojaonline/hermanos/br/models/dto/produto/DadosAtualizaProduto.java
@@ -2,10 +2,10 @@ package com.lojaonline.hermanos.br.models.dto.produto;
 
 import com.lojaonline.hermanos.br.models.Produto;
 
-public record DadosListagemProduto(Long id, String nome, String descricao, Double preco, Integer qtdDisponivel, String categoria) {
-    public DadosListagemProduto(Produto produto) {
+public record DadosAtualizaProduto(String nome, String descricao, Double preco, Integer qtdDisponivel, String categoria) {
+
+    public DadosAtualizaProduto(Produto produto) {
         this(
-                produto.getId(),
                 produto.getNome(),
                 produto.getDescricao(),
                 produto.getPreco(),
