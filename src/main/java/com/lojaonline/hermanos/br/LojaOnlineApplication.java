9aabaf388d396246ad980314b6cbbdb60479b6c3
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-08-30 20:24:38 -0300

    Tirado dependências não utilizadas

--- src/main/java/com/lojaonline/hermanos/br/LojaOnlineApplication.java
+++ src/main/java/com/lojaonline/hermanos/br/LojaOnlineApplication.java
@@ -1,7 +1,5 @@
 package com.lojaonline.hermanos.br;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 	Projeto: Loja Online
  */
 
-@OpenAPIDefinition(info = @Info(title = "Loja Online", version = "1", description = "Essa é uma api que tem como objetivo, o gerenciamento dos dados de uma loja online"))
 @SpringBootApplication
 public class LojaOnlineApplication {
 
