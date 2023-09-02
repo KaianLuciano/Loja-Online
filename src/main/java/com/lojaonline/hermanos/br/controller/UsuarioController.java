5fcd142b224b493cde25fa62a9432403a31340a8
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-09-02 18:07:53 -0300

    Alterado end-point findById da controller UsuarioController

--- src/main/java/com/lojaonline/hermanos/br/controller/UsuarioController.java
+++ src/main/java/com/lojaonline/hermanos/br/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.lojaonline.hermanos.br.controller;
 
 
 import com.lojaonline.hermanos.br.models.Usuario;
+import com.lojaonline.hermanos.br.models.dto.usuario.DadosListagemUsuario;
 import com.lojaonline.hermanos.br.repository.UsuarioRepository;
 import com.lojaonline.hermanos.br.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,31 +25,25 @@ public class UsuarioController {
 
     @Operation(summary = "Procura todos os usuario do banco")
     @GetMapping
-    public ResponseEntity<Object> findAll(){
+    public ResponseEntity<List<DadosListagemUsuario>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(usuarioService.findAll());
     }
 
     @Operation(summary = "Procura no banco o usuario que representa o id passado")
     @GetMapping(value = "/{cpfUsuario}")
-    public ResponseEntity<Object> findById(@PathVariable(value = "cpfUsuario") String cpfUsuario) {
-        Optional<Usuario> usuarioModelsOptional = usuarioService.findByIdPrivate(cpfUsuario);
-
-        if(usuarioModelsOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
-        }
-
+    public ResponseEntity<DadosListagemUsuario> findById(@PathVariable(value = "cpfUsuario") String cpfUsuario) {
         return ResponseEntity.status(HttpStatus.OK).body(usuarioService.findById(cpfUsuario));
     }
 
     @Operation(summary = "Cria um novo usuario")
     @PostMapping
-    public ResponseEntity<Object> saveUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<DadosListagemUsuario> saveUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.saveUsuario(usuario));
     }
 
     @Operation(summary = "Atualiza o usuario que representa o id passado")
     @PutMapping("/{cpfUsuario}")
-    public ResponseEntity<Object> updateUsuario(@PathVariable(value = "cpfUsuario") String cpfUsuario, @RequestBody Usuario usuario) {
+    public ResponseEntity<DadosListagemUsuario> updateUsuario(@PathVariable(value = "cpfUsuario") String cpfUsuario, @RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.OK).body(usuarioService.updateUsuario(cpfUsuario, usuario));
     }
 
