deaf09e1de28199b243bfe055a703c56a99d6824
Kaian Luciano Alves Dos Santos <112596060+KaianLuciano@users.noreply.github.com>
2023-10-04 22:44:04 -0300

    Criado método validateToken

--- src/main/java/com/lojaonline/hermanos/br/config/TokenService.java
+++ src/main/java/com/lojaonline/hermanos/br/config/TokenService.java
@@ -3,6 +3,7 @@ package com.lojaonline.hermanos.br.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.lojaonline.hermanos.br.models.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
-                    .withIssuer("auth-api")
+                    .withIssuer("api-loja-online")
                     .withSubject(usuario.getEmail())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
@@ -30,7 +31,19 @@ public class TokenService {
             throw new RuntimeException("Erro while generating token: ", exception);
         }
     }
-    
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("api-loja-online")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
 
     private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
